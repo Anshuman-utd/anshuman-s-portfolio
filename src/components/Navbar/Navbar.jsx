329090f353a48203fc0d971b2dc1dd9f428725d8
Anshuman-utd <anshumanmehta11@gmail.com>
2025-04-15 16:50:38 +0530

    Updated navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState,useRef } from 'react'
 import './Navbar.css'
-import logo from '../../assets/logo.svg'
+import logo from '../../assets/logo1.png'
 import underline from '../../assets/nav_underline.svg'
 import AnchorLink from 'react-anchor-link-smooth-scroll'
 import menu_open from '../../assets/menu_open.svg'
@@ -19,7 +19,7 @@ const Navbar = () => {
   return (
 
     <div className='navbar'>
-        <img src = {logo} alt = ""/>
+        <img src = {logo} alt = "" className='logo-img'/>
         <img src={menu_open} onClick = {openMenu} alt = "" className='nav-mob-open'/>
         <ul ref={menuRef}  className="nav-menu">
             <img src={menu_close} onClick = {closeMenu} alt="" className="nav-mob-close" />
